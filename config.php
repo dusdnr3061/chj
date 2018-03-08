e2741d49f488bd6c48deacb933ca9304d438aca2
김연욱 <37171333+dusdnr3061@users.noreply.github.com>
2018-03-08 18:34:33 +0900

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+$host="localhost";
+$user="root";
+$password="cjdgowls";
+$db="chj";
+
+$mysqli = mysqli_connect($host,$user,$password,$db);
+*/
+$con = mysqli_connect("localhost","root","cjdgowls","chj","3306");
+mysqli_set_charset($con,"utf8");
+?>
